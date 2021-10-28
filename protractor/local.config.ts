d4cdbf07bf4221cf1000ef53298dca6876b9564c
DanielDi <despinalm@unal.edu.co>
2021-10-28 12:58:25 -0500

    "point 7"

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -1,9 +1,10 @@
 import { Config, browser } from 'protractor';
+import { reporter } from '../helpers/reporter';
 
 export const config: Config = {
   framework: 'jasmine',
   specs: [ '../test/google.spec.js' ],
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  SELENIUM_PROMISE_MANAGER: 'false',
   capabilities: {
     browserName: 'chrome',
     count: 1,
@@ -13,5 +14,6 @@ export const config: Config = {
   },
   onPrepare: () => {
     browser.ignoreSynchronization = true;
+    reporter();
   }
 };
