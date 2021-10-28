545fc76bd2cade30527489c39440fc6d9e21b008
Daniel <despinalm@unal.edu.co>
2021-10-28 13:31:14 -0500

    route corrected

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -1,10 +1,10 @@
 import { Config, browser } from 'protractor';
-import { reporter } from '../helpers/reporter';
+import { reporter } from './helpers/reporter';
 
 export const config: Config = {
   framework: 'jasmine',
   specs: [ '../test/google.spec.js' ],
-  SELENIUM_PROMISE_MANAGER: 'false',
+  SELENIUM_PROMISE_MANAGER: false,
   capabilities: {
     browserName: 'chrome',
     count: 1,
